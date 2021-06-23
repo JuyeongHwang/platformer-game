a4cc355aeaae14c454754746cedc113611c5957e
JuyeongHwang <49023736+JuyeongHwang@users.noreply.github.com>
2021-06-23 09:27:00 +0900

    0623

--- python-pymunk/0622tutorial.py
+++ python-pymunk/0622tutorial.py
@@ -1,6 +1,9 @@
 import pygame,sys
 import pymunk
 
+
+#6/22 볼 class 추가
+#6/23 볼 image 추가
 pygame.init()
 display = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
@@ -14,9 +17,10 @@ Dynamic, kinematic, static
 parameter : (mass,moment,body_type)
 '''
 
+ball_radius = 30
 body = pymunk.Body()
 body.position = 400,400
-shape = pymunk.Circle(body,10)
+shape = pymunk.Circle(body,ball_radius)
 shape.density = 1
 shape.elasticity = 1
 space.add(body, shape)
@@ -26,6 +30,8 @@ segment_shape = pymunk.Segment(segment_body,(0,550),(800,700),5)
 segment_shape.elasticity = 1
 space.add(segment_body,segment_shape) 
 
+image = pygame.image.load("apple_item.png")
+image = pygame.transform.scale(image,(ball_radius*2,ball_radius*2))
 
 def game():
     running = True
@@ -37,7 +43,8 @@ def game():
                 sys.exit()
         
         display.fill((255,255,255))
-        pygame.draw.circle(display,(255,0,0),body.position,10)
+        #pygame.draw.circle(display,(255,0,0),body.position,10)
+        display.blit(image,(body.position.x-ball_radius,body.position.y-ball_radius))
         pygame.draw.line(display,(0,0,0),(0,550),(800,700),5)
         pygame.display.update()
         clock.tick(FPS)
